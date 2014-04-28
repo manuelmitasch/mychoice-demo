72544ff59099e7991a8b4cd2408ccd1e93d33896
Manuel Mitasch <manuel@cms.mine.nu>
2014-04-28 16:09:17 +0200

    Show all array items of quizzes

--- js/app.js
+++ js/app.js
@@ -15,6 +15,18 @@ App.IndexRoute = Ember.Route.extend({
   }
 });
 
+App.QuizzesRoute = Ember.Route.extend({
+  model: function() {
+    return [
+      { id: 1, name: 'Fun with flags #1', tags: [1], owner: 1 },
+      { id: 2, name: 'Fun with flags #2', tags: [1], owner: 2 },
+      { id: 3, name: 'Fun with flags #3', tags: [1], owner: 1 },
+      { id: 4, name: 'French vocabulary #1', tags: [1,2], owner: 1 },
+      { id: 4, name: 'Service Engineering Exam SS2014', tags: [3], owner: 1 }
+    ]
+  }
+});
+
 App.ApplicationController = Ember.Controller.extend({
   needs: 'user-profile',
   user: Ember.computed.alias("controllers.user-profile")
