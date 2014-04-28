10233ce98d6ad9f37d1107fcdbaaeccc03e822f9
Manuel Mitasch <manuel@cms.mine.nu>
2014-04-28 16:00:38 +0200

    Bind user profile form to data

--- js/app.js
+++ js/app.js
@@ -15,3 +15,18 @@ App.IndexRoute = Ember.Route.extend({
   }
 });
 
+App.UserProfileController = Ember.ObjectController.extend({
+  id: 1,
+  userName: 'manuel_mitasch',
+  firstName: "Manuel",
+  lastName: "Mitasch",
+  fullName: function() {
+    return this.get('firstName') + ' ' + this.get('lastName');
+  }.property('firstName', 'lastName'),
+
+  actions: {
+    saveUser: function() {
+      alert("Changes saved");
+    }
+  }
+});
